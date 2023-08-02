2ae0ed7b214e2faab2cd293103efe1d1db38b7b8
james-hepworth <james.hepworth@uct.ac.za>
2023-08-02 16:26:29 +0200

    hardcode api and field

--- scripts.js
+++ scripts.js
@@ -2,10 +2,10 @@ function incrementValue() {
     const channelId = 2232135; // Replace YOUR_CHANNEL_ID with your actual ThingSpeak channel ID
     const apiKey = 'VD10YXRPSWG8P0O2'; // Replace YOUR_API_KEY with your actual ThingSpeak API Write Key
   
-    const url = 'https://api.thingspeak.com/update.json';
+    //const url = 'https://api.thingspeak.com/update.json';
     const value = 0; // The value you want to increment (you can change this as needed)
   
-    fetch(`${url}?api_key=${apiKey}&field1=${value}`)
+    fetch(`https://api.thingspeak.com/update?api_key=VD10YXRPSWG8P0O2&field1=0`)
       .then((response) => {
         if (!response.ok) {
           throw new Error('Network response was not ok');
