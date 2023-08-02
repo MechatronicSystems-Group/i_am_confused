d9fc5c91db515b398ac1a5fe93a5ca73fa85683d
james-hepworth <james.hepworth@uct.ac.za>
2023-08-02 13:41:46 +0200

    changed url to string

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ function incrementValue() {
     const channelId = 2232135; // Replace YOUR_CHANNEL_ID with your actual ThingSpeak channel ID
     const apiKey = 'VD10YXRPSWG8P0O2'; // Replace YOUR_API_KEY with your actual ThingSpeak API Write Key
   
-    const url = `https://api.thingspeak.com/update.json`;
+    const url = 'https://api.thingspeak.com/update.json';
     const value = 0; // The value you want to increment (you can change this as needed)
   
     fetch(`${url}?api_key=${apiKey}&field1=${value}`)
